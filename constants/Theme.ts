ad930475c642291196d0e37e2b0a354d628b5b78
Caroline Cagnin <cagnin.caroline@gmail.com>
2023-09-26 19:24:35 -0300

    init project

---
+++ constants/Theme.ts
@@ -0,0 +1,19 @@
+export default {
+
+  colors: {
+    primary: '047460',
+    secondary: 'EB485C',
+    lightGreen: 'D4FFDC',
+    lightBrown: 'C3AA90',
+    background: '001627'
+  },
+
+  text : {
+    xSmall: 13,
+    small: 14,
+    medium: 16,
+    large: 20,
+    xLarge: 24,
+    xxLarge: 32,
+  },
+};
