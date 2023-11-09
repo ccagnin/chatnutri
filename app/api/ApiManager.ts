ff9ccc0f3a96f256a7c4b221186a77b7c2a8fb15
Caroline Cagnin <cagnin.caroline@gmail.com>
2023-11-09 15:33:46 -0300

    mercado pago

--- app/api/ApiManager.ts
+++ app/api/ApiManager.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const ApiManager = axios.create({
-  baseURL: 'http://192.168.1.2:3000/',
+  baseURL: 'http://192.168.15.4:3333/',
   responseType: 'json',
   withCredentials: true,
 });
