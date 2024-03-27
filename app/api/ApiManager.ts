da92ead51799f9c26136065d49aa01f860aed948
Rodrigo <rod.lemon@hotmail.com>
2024-03-26 23:17:32 -0300

    ajustando FE do pagamento, ainda faltando implementar chamadas para BE

--- app/api/ApiManager.ts
+++ app/api/ApiManager.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const ApiManager = axios.create({
-  baseURL: 'http://192.168.0.6:3333/',
+  baseURL: 'http://192.168.0.7:3333/',
   responseType: 'json',
   withCredentials: true,
 });
 
 export const ApiStripe = axios.create({
-  baseURL: 'http://192.168.0.6:4000/',
+  baseURL: 'http://192.168.0.7:4000/',
   responseType: 'json',
   withCredentials: true,
 });
--- app/screens/stripe/PaymentScreen.tsx
+++ app/screens/stripe/PaymentScreen.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { View, Button, TextInput, Alert, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Button, TextInput, Alert, Text, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
 import { CardField, StripeProvider, useStripe, CardForm, initStripe } from '@stripe/stripe-react-native';
 import PlatformPayLocalButton from './PlatformPayLocalButton';
 import { ApiStripe } from '../../api/ApiManager';
+import { FontAwesome } from '@expo/vector-icons';
 
 initStripe({
     publishableKey: 'sua_chave_publica_stripe',
@@ -10,6 +11,25 @@ initStripe({
 
 function getPlans() {
     //ApiStripe.get()
+}
+
+
+function CardSelectPlan({ props, isSelected, handlePlanSelected }) {
+
+    return (
+        <TouchableOpacity style={isSelected == props.id ? styles.selectedCard : styles.cardContainer} onPress={() => handlePlanSelected(props.id)}>
+            {isSelected == props.id && <FontAwesome name="check-circle" size={24} color="green" style={{
+                position:'absolute',
+                right: 5,
+                top: 5,
+            }} />}
+            <Text style={styles.planText}>{props.name}</Text>
+            <Text style={styles.valuePlanText}>{props.value.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+            })}</Text>
+        </TouchableOpacity>
+    );
 }
 
 function PaymentScreen() {
@@ -22,7 +42,28 @@ function PaymentScreen() {
         cvc: '',
     });
     const [cardDetails, setCardDetails] = useState({});
-    const [cardName, setCardName] = useState('');
+    const [cardName, setCardName] = useState();
+
+    const [selectedLanguage, setSelectedLanguage] = useState();
+
+    const [plans, setPlans] = useState([
+        { id: 1, name: 'Plano semanal', value: 5 },
+        { id: 2, name: 'Plano mensal', value: 20 },
+        { id: 3, name: 'Plano anual', value: 240 },
+    ])
+
+    const [plansList, setPlansList] = useState([]);
+
+    const [selectedPlan, setSelectedPlan] = useState(null)
+
+    const handlePlanSelected = (plan_id) => {
+        if (plan_id === selectedPlan) {
+            setSelectedPlan(null)
+            return
+        }
+        setSelectedPlan(plan_id)
+    }
+
 
     const handleFormComplete = async () => {
         try {
@@ -52,6 +93,16 @@ function PaymentScreen() {
             publishableKey="pk_test_XKUpwPvvEnNxMsSzoLm8H3i8"
         >
             <View style={styles.container}>
+            <View>
+                <Text style={styles.labelPayment}>Selecione o plano</Text>
+                <View style={styles.plansContainer}>
+                    <CardSelectPlan props={plans[0]} isSelected={selectedPlan} handlePlanSelected={handlePlanSelected} />
+                    <CardSelectPlan props={plans[1]} isSelected={selectedPlan} handlePlanSelected={handlePlanSelected} />
+                    <CardSelectPlan props={plans[2]} isSelected={selectedPlan} handlePlanSelected={handlePlanSelected} />
+                </View>
+
+            </View>
+            <View style={{ height: 400 }}>
                 {/* <View style={styles.googlePayContainer}>
                     <View><Text style={styles.labelPayment}>Pagar com Google pay</Text></View>
                     <PlatformPayLocalButton />
@@ -63,42 +114,52 @@ function PaymentScreen() {
                     onChangeText={(val) => setForm({ ...form, cardName: val })}
                     value={cardName}
                 />
-                <CardForm
-                    placeholders={{
-                        number: 'Numero do cartão',
+                <CardField
+                    cardStyle={{
+                        backgroundColor: '#FFFFFF',
+                        textColor: '#000000',
                     }}
-                    defaultValues={{ countryCode: 'BR' }}
+                    style={styles.containerCreditCardPayment}
+                    onCardChange={(cardDetails) => {
+                        console.log('cardDetails', cardDetails);
+                    }}
                     postalCodeEnabled={false}
-                    style={{
-                        width: '100%',
-                        height: 200,
-                    }}
-                    onFormComplete={handleFormComplete}
                 />
                 <Button
                     title="Pagar"
                 //onPress={handlePayment}
                 />
             </View>
+            </View>
         </StripeProvider>
     );
 }
 
-export default PaymentScreen;
+function Content() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <View style={{ flex: 1 }}>
+                    <PaymentScreen />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default Content;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        paddingHorizontal: '5%',
+        paddingHorizontal: '2%',
+        paddingVertical: '5%',
     },
     input: {
         height: 40,
         width: '100%',
         borderColor: 'gray',
         borderWidth: 1,
-        marginBottom: 20,
         paddingHorizontal: 10,
     },
     labelPayment: {
@@ -111,4 +172,43 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    containerCreditCardPayment: {
+        width: '100%',
+        height: 50,
+        marginVertical: 30,
+    },
+    plansContainer:{
+        flex: 1,
+        flexDirection: 'row',
+    },
+    cardContainer: {
+        paddingHorizontal:10,
+        height: 100,
+        width: '30%',
+        justifyContent: "center",
+        alignItems: "center",
+        marginHorizontal: 5,
+        marginBottom: '5%',
+        borderColor:'black',
+        borderWidth:.5,
+        borderRadius: 14,
+    },
+    selectedCard: {
+        paddingHorizontal:10,
+        width: '30%',
+        justifyContent: "center",
+        alignItems: "center",
+        marginHorizontal: 5,
+        marginBottom: '5%',
+        borderWidth:.5,
+        borderRadius: 14,
+        opacity: 0.5,
+    },
+    planText: {
+        fontSize: 14,
+    },
+    valuePlanText: {
+        fontSize: 16,
+        fontWeight: "bold",
+    }
 })
