a660e0a660d80783f595d1b6199ace43a61d8c04
Rodrigo <rod.lemon@hotmail.com>
2024-05-17 00:05:21 -0300

    chore: atualizando caminho do save-subscription

--- app/utils/subscriptionMethodsStripe.js
+++ app/utils/subscriptionMethodsStripe.js
@@ -1,19 +1,22 @@
 import { Alert } from 'react-native';
+import { ApiManager } from './ApiManager';
+
 // Função para salvar a assinatura no servidor backend e associar o subscriptionId ao ID do usuário
 const saveSubscription = async (subscriptionId) => {
     try {
         const userId = await SecureStore.getItemAsync('userId');
-        const response = await fetch('http://rota.com/save-subscription', { //Essa rota é a da API principal, não da API de pagamentos
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                subscriptionId,
-                userId: userId,
-            }),
-        });
-        if (!response.ok) {
+        const response = await ApiManager.post('/save-subscription', //Essa rota é a da API principal, não da API de pagamentos
+        {
+            subscriptionId,
+            userId: userId,
+        },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        )
+        if (response.status < 200 && response.status >= 300) {
             throw new Error('Erro ao salvar a assinatura no banco de dados');
         }
     } catch (error) {
